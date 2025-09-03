76f1e78e8a8fae3771eb356a64691c5f53d040d0
koti <devakoti.fullstackdeveloper@gmail.com>
2025-09-03 11:40:48 +0530

    db updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
+  origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
